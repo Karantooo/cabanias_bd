069e38f343664712a26f6dbe77cf99d0dc01a2e5
Karantooo <ctomasalvarezn@gmail.com>
2024-06-13 23:19:50 -0400

    feat: ultimos cambios

--- consulta_disponibilidad.sql
+++ consulta_disponibilidad.sql
@@ -1,12 +1,17 @@
 CREATE OR REPLACE FUNCTION ObtenerSecuencia(inicio DATE, fin DATE, servicio_nombre VARCHAR(1000))
-RETURNS TABLE (servicio VARCHAR(100) , folio INT) AS $$
+RETURNS TABLE (servicio VARCHAR(100) , folio_RET INT) AS $$
 BEGIN
     RETURN QUERY 
 	SELECT servicio.nombre, folio 
 	FROM reserva
 	INNER JOIN servicio on reserva.id_servicio = servicio.id_servicio
-	WHERE (fecha_inicio = inicio) AND (fecha_fin = fin) AND (servicio.nombre = servicio_nombre);
+	WHERE (inicio >= fecha_inicio) AND (fin <= fecha_fin) AND (servicio.nombre = servicio_nombre);
 END;
 $$ LANGUAGE plpgsql;
 
 
+SELECT * FROM reserva;
+SELECT * FROM servicio;
+
+
+SELECT * FROM ObtenerSecuencia('2024-07-01', '2024-07-02', 'Quincho Grande');
